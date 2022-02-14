a7c110290c918836b753a9286efc406fe289e062
Robert Milijaš <robert.milijas@fer.hr>
2022-02-14 13:50:16 +0100

    Read sensor_id

--- read_sensor.py
+++ read_sensor.py
@@ -24,7 +24,7 @@ else:
 
 output_file.close()
 
-print("h, min, sec, pm_25, pm_10")
+print("h, min, sec, id, pm_25, pm_10")
 
 while True:
     print("Waiting for data:");
@@ -36,9 +36,12 @@ while True:
     pm_25 = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
     pm_10 = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
 
+    sensor_id = int.from_bytes(b''.join(data[6:8]), byteorder='little')
+
     now = datetime.now()
     output_string = str(now.hour) + ', ' + str(now.minute) + ', ' \
-      + str(now.second) + ', ' + str(pm_25) + ', ' + str(pm_10) + '\n'
+      + str(now.second) + ', ' + str(sensor_id) + ', ' \
+      + str(pm_25) + ', ' + str(pm_10) + '\n'
 
     output_file = open(output_filename, "a+");
     output_file.write(output_string)
